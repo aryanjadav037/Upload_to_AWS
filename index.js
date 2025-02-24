99883d9fc78dd4f425b8a87632e3a462fcf5a3c3
aryan <aryanjadav081103@gmail.com>
2025-02-24 09:59:21 +0530

    init

---
+++ index.js
@@ -0,0 +1,132 @@
+import { LambdaClient, InvokeCommand } from "@aws-sdk/client-lambda";
+import { S3Client, GetObjectCommand,PutObjectCommand } from "@aws-sdk/client-s3";
+import { config } from "dotenv";
+import express from "express";
+import multer from "multer";
+import fs from "fs";
+import { data } from "./file.js";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
+config();
+
+const app = express();
+app.set("view engine", "ejs");
+
+const upload = multer({ dest: "uploads/" });
+
+const lambda_client = new LambdaClient({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+const s3_client = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+});
+
+app.get("/", (req, res) => {
+    res.render("index", { message: null, response: null });
+});
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+    try {
+        if (!req.file) {
+            return res.render("index", { message: "No file uploaded!", response: null });
+        }
+
+        const fileBuffer = fs.readFileSync(req.file.path);
+
+        const encodedFile = fileBuffer.toString("base64");
+
+        const input = {
+            FunctionName: "test22125",
+            InvocationType: "RequestResponse",
+            Payload: new TextEncoder().encode(JSON.stringify({ file: encodedFile })),
+        };
+
+        const command = new InvokeCommand(input);
+        const response = await lambda_client.send(command);
+
+        const decodedPayload = new TextDecoder().decode(response.Payload);
+
+        res.render("index", {
+            message: "Lambda Invoked Successfully",
+            response: JSON.parse(decodedPayload)
+        });
+
+    } catch (error) {
+        console.error("Error invoking Lambda:", error);
+        res.render("index", {
+            message: "Lambda invocation failed",
+            response: { error: error.message }
+        });
+    }
+});
+
+app.post("/url", async (req, res) => {
+    try {
+        const signedUrl = await getSignedUrl(s3_client, new PutObjectCommand({
+            Bucket: process.env.AWS_BUCKET_NAME,
+            Key: "7mb.json",
+            ContentType: "json"
+
+        }));
+
+        const input = {
+            FunctionName: "test22125",
+            InvocationType: "RequestResponse",
+            Payload: JSON.stringify(signedUrl)
+        };
+
+        const command = new InvokeCommand(input);
+        const response = await lambda_client.send(command);
+
+        const decodedPayload = new TextDecoder().decode(response.Payload);
+        // const jsonResponse = JSON.parse(decodedPayload);
+
+        console.log(decodedPayload)
+        res.send(decodedPayload);
+
+    } catch (error) {
+        console.error("Error invoking Lambda:", error);
+        res.render({
+            message: "Lambda invocation failed",
+            response: { error: error.message }
+        });
+    }
+});
+
+app.post("/upload-v2", async (req, res) => {
+    try {
+        const input = {
+            FunctionName: "test22125",
+            InvocationType: "RequestResponse",
+            Payload: JSON.stringify(signedUrl)
+        };
+
+        const command = new InvokeCommand(input);
+        const response = await lambda_client.send(command);
+
+        const decodedPayload = new TextDecoder().decode(response.Payload);
+        // const jsonResponse = JSON.parse(decodedPayload);
+
+        console.log(decodedPayload)
+        res.send(decodedPayload);
+
+    } catch (error) {
+        console.error("Error invoking Lambda:", error);
+        res.render({
+            message: "Lambda invocation failed",
+            response: { error: error.message }
+        });
+    }
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
